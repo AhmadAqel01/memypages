8287b7ea39fd5db99363040c5205952543dbc9bf
Ahmad Aqel <129675023+AhmadAqel01@users.noreply.github.com>
2025-05-11 14:24:08 -0500

    Update SelectorsToBeRemoved.js

--- WebCleaners/SelectorsToBeRemoved.js
+++ WebCleaners/SelectorsToBeRemoved.js
@@ -10,15 +10,19 @@ copy(`const selectors_to_add = ${JSON.stringify($$('.cm-variable').map(e=>e.inne
 */
 
 //#region To update content, replace this section:
-const selectors_to_add = []
+const selectors_to_add = ["aaaaaaaaaaaaaaa"]
 const current_selectors_to_remove = ["a.abstract_t",".ack",".acknowledgment",".acknowledgments","[class*=\"ads\"]","a.medical_review","#article-references",".article-section__references","[class*=\"aside\"]","aside","*[alt*='audio']","*[class*='audio']","*[data-*='audio']","*[href*='audio']","*[id*='audio']","*[name*='audio']","*[src*='audio']","*[title*='audio']","[data-article-section-title=\"Author Affiliations\"]","[data-article-section-title=\"Author Information\"]",".authorSectionElem",".bibLink",".bibr","button.j-inline-reference","button.j-inline-reference",".c-ckc-acknowledgments",".c-ckc-further-reading","[ck-no-html-print]","#closeMessage","div.collapse-sideways-arrow","div.headingAnchor","div.star-rating__container","div#topic-toolbar","div.x-fixed-bar",".fig-orig","figure>.inline-image-caption","[class*=\"footer\"]","footer",".footnote",".footnote-backref",".footnote-ref","[data-article-section-title='Funding and Disclosures']",".grecaptcha-badge","header",".headingEndMark",".hide",".icon-expand",".inline-icon",".inline-image-caption:not(:is(.caption-holder *))",".inline-table-caption:has(a)",".inline-table-caption:has(i)","isDesktop.utdWkHeader.topicView",".isDesktop.utdWkHeader.topicView#topicContainer#topicOutline",".letterdrop-custom-embed","[class^=\"licensing\"]","link",".list-item-label","[data-once-text=\"Messages.content_loading_error\"]",".MuiLink-underlineHover","[class*=\"nav\"]","nav",".ng-hide","[rel=\"noopener noreferrer\"]",".overflowTopicDropdown",".paywall",".reference","[data-type='Reference']",".reference-citations","#reference-list",".references",".References","#references","#ref-list-a.h.b","review",".screenreader-text","script","section.c-ckc-bibliography","section.c-ckc-bibliography","[data-behavior='ShowPopupTip']","#slaMessageContainer","style","sup:has(a.ejp-citation-link)","#tipsAndLinks","#topicAgreement","topicContainer","#topicContributors","#topicContributors","#topicOutline","topic-toolbar","#topicVersionRevision",".tsec.sec","#ui-ncbiinpagenav-1>div.fm-sec.half_rhythm.no_top_margin","[class^='updateInfoContainer']","#utd-main>utd-bridge-topic-view>div>div.topic-section-pointer.wkce-icon-arrow-right.topic-section-pointer-animate-in","[class*=\"video\"]","*[alt*='video']","*[class*='video']","*[data-*='video']","*[href*='video']","*[id*='video']","*[name*='video']","*[src*='video']","*[title*='video']",".visuallyhidden",".x-outline-menu",".x-outline-menu-button",".xref-bibr"]
 /* Output as one css selector:
 a.abstract_t, .ack, .acknowledgment, .acknowledgments, [class*="ads"], a.medical_review, #article-references, .article-section__references, [class*="aside"], aside, *[alt*='audio'], *[class*='audio'], *[data-*='audio'], *[href*='audio'], *[id*='audio'], *[name*='audio'], *[src*='audio'], *[title*='audio'], [data-article-section-title="Author Affiliations"], [data-article-section-title="Author Information"], .authorSectionElem, .bibLink, .bibr, button.j-inline-reference, button.j-inline-reference, .c-ckc-acknowledgments, .c-ckc-further-reading, [ck-no-html-print], #closeMessage, div.collapse-sideways-arrow, div.headingAnchor, div.star-rating__container, div#topic-toolbar, div.x-fixed-bar, .fig-orig, figure>.inline-image-caption, [class*="footer"], footer, .footnote, .footnote-backref, .footnote-ref, [data-article-section-title='Funding and Disclosures'], .grecaptcha-badge, header, .headingEndMark, .hide, .icon-expand, .inline-icon, .inline-image-caption:not(:is(.caption-holder *)), .inline-table-caption:has(a), .inline-table-caption:has(i), isDesktop.utdWkHeader.topicView, .isDesktop.utdWkHeader.topicView#topicContainer#topicOutline, .letterdrop-custom-embed, [class^="licensing"], link, .list-item-label, [data-once-text="Messages.content_loading_error"], .MuiLink-underlineHover, [class*="nav"], nav, .ng-hide, [rel="noopener noreferrer"], .overflowTopicDropdown, .paywall, .reference, [data-type='Reference'], .reference-citations, #reference-list, .references, .References, #references, #ref-list-a.h.b, review, .screenreader-text, script, section.c-ckc-bibliography, section.c-ckc-bibliography, [data-behavior='ShowPopupTip'], #slaMessageContainer, style, sup:has(a.ejp-citation-link), #tipsAndLinks, #topicAgreement, topicContainer, #topicContributors, #topicContributors, #topicOutline, topic-toolbar, #topicVersionRevision, .tsec.sec, #ui-ncbiinpagenav-1>div.fm-sec.half_rhythm.no_top_margin, [class^='updateInfoContainer'], #utd-main>utd-bridge-topic-view>div>div.topic-section-pointer.wkce-icon-arrow-right.topic-section-pointer-animate-in, [class*="video"], *[alt*='video'], *[class*='video'], *[data-*='video'], *[href*='video'], *[id*='video'], *[name*='video'], *[src*='video'], *[title*='video'], .visuallyhidden, .x-outline-menu, .x-outline-menu-button, .xref-bibr
 */
-
-
 //#endregion
-  
+
+function mergeUniqueStrings(arr1, arr2) {
+  // Spread both arrays into a new Set to automatically handle duplicates,
+  // then spread the Set back into an array.
+  return [...new Set([...arr1, ...arr2])];
+}
+
 function sortStringsAdvanced(stringArray) {
   // Helper function to determine the primary sorting key for a string
   const getProcessedKey = (str) => {
@@ -57,13 +61,13 @@ function sortStringsAdvanced(stringArray) {
 
   return stringArray; // Return the sorted array (though it's sorted in-place)
 }
-
-const sorted_selectors = sortStringsAdvanced([...current_selectors_to_remove]);
+merged_selectors = mergeUniqueStrings(current_selectors_to_remove,selectors_to_add)
+const sorted_selectors = sortStringsAdvanced([...merged_selectors]);
 
 const final_output = `const selectors_to_add = []
 const current_selectors_to_remove = ${JSON.stringify(sorted_selectors)}
 /* Output as one css selector:
-${current_selectors_to_remove.join(', ')}
+${sorted_selectors.join(', ')}
 
 */`
 
